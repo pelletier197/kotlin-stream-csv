9f3e602028ffeb4e125de07798065e2dfaeeb879
John power <sunny.pelletier@qohash.com>
2020-09-06 12:43:02 -0400

    add raw and header csv parser

--- src/main/kotlin/com/kheops/csv/reader/Reader.kt
+++ src/main/kotlin/com/kheops/csv/reader/Reader.kt
@@ -45,91 +45,7 @@ data class TypedCsvSchema<T>(
     }
 }
 
-class RawCsvReader(
-    val separator: String = ",",
-    val delimiter: String = "\"",
-    val trimEntries: Boolean = false,
-    val skipEmptyLines: Boolean = true
-) {
-    private val regex = buildSplitRegex()
 
-    fun read(url: URL, charset: Charset = StandardCharsets.UTF_8): Stream<RawCsvLine> {
-        return read(url.openStream(), charset)
-    }
-
-    fun read(input: InputStream, charset: Charset = StandardCharsets.UTF_8): Stream<RawCsvLine> {
-        return read(BufferedReader(InputStreamReader(input, charset)).lines())
-    }
-
-    fun read(file: File): Stream<RawCsvLine> {
-        return read(file.toPath())
-    }
-
-    fun read(path: Path): Stream<RawCsvLine> {
-        return read(Files.lines(path))
-    }
-
-    fun read(lines: List<String>): Stream<RawCsvLine> {
-        return read(lines.stream())
-    }
-
-    fun read(lines: Stream<String>): Stream<RawCsvLine> {
-        val skippedEmptyLines = if (skipEmptyLines) lines.filter { it.isNotBlank() } else lines
-        return processLines(skippedEmptyLines)
-    }
-
-    private fun processLines(
-        lines: Stream<String>,
-    ): Stream<RawCsvLine> {
-        val index = AtomicInteger(1)
-        return lines.map {
-            RawCsvLine(
-                columns = regex.findAll(it).toList().map { v -> format(v.groupValues.last()) },
-                line = index.getAndIncrement(),
-            )
-        }
-    }
-
-    private fun format(value: String): String {
-        var output = removeDelimiters(value)
-
-        if (trimEntries) {
-            output = output.trim()
-        }
-
-        return output
-    }
-
-    private fun removeDelimiters(value: String): String {
-        val trimmed = value.trim()
-        if (trimmed.startsWith(delimiter) && trimmed.endsWith(delimiter)) {
-            return trimmed.drop(1).dropLast(1)
-        }
-        return trimmed
-    }
-
-    private fun buildSplitRegex(): Regex {
-        val s = separator
-        val d = delimiter
-        // https://stackoverflow.com/questions/18144431/regex-to-split-a-csv/18147076
-        return Regex("(?:$s|\\n|^)($d(?:(?:$d$d)*[^$d]*)*$d|[^$d$s\\n]*|(?:\\n|\$))")
-    }
-}
-
-data class HeaderCsvReader(
-    val header: List<String>? = null,
-    val errorOnMissingProperties: Boolean = true,
-    val reader: RawCsvReader = RawCsvReader(),
-) {
-    fun withHeader(header: List<String>): HeaderCsvReader {
-        return copy(header = header)
-    }
-}
-
-data class RawCsvLine(
-    val columns: List<String>,
-    val line: Int
-)
 
 data class CsvLine<T>(
     val result: T?,
@@ -139,23 +55,17 @@ data class CsvLine<T>(
     val hasErrors: Boolean get() = !errors.isNullOrEmpty()
 
     fun getResultOrThrow(): T {
-        return result ?: throw CsvParsingException(
+        return if (hasErrors) throw CsvParsingException(
             message = "error while parsing CSV file",
             errors = errors!!,
+            line = line
+        ) else result ?: throw CsvParsingException(
+            message = "expected a non null CSV result",
+            errors = listOf(CsvError.NON_NULL_RESULT_EXPECTED),
             line = line
         )
     }
 }
-
-enum class CsvError {
-    TEST
-}
-
-data class CsvParsingException(
-    override val message: String,
-    val errors: List<CsvError>,
-    val line: Int
-) : Exception(message)
 
 fun main() {
     val res = RawCsvReader().read(listOf("""a,b,"c",d""", """r,e,e,e,"","a ", sdsdfsd """).stream())
